cd4f428c1beec82ccc3a6501332e593045fce0b8
segitiga <rafi.nizar15@gmail.com>
2020-01-01 21:58:06 +0700

    Update welcome.blade.php
    added bootstrap

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -8,15 +8,17 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Welcome!</title>
 
     <style>
 
+        .active {
+            background: #4FA9DC;
+            color: #000;
+        }
 
-    
-    
     </style>
-    
+
   </head>
   <body>
 
@@ -24,27 +26,27 @@
 
     <div id="app" class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">myBlog</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div id="navbarNavDropdown" class="navbar-collapse collapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="/about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="/contact">Contact</a>
                     </li>
     
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Dropdown
+                          My Account
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="#">Action</a>
@@ -52,14 +54,75 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                        <a class="nav-link" href="{{ url('/login') }}">link</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                        <a class="nav-link" href="{{ url('/register') }}">link</a>
                     </li>
                 </ul>
             </div>
         </nav>
+    </div>
+
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h1 class="display-4">Welcome to my Blog!</h1>
+                    <p class="lead">Thank you for visiting</p>
+                    <hr class="my-4">
+                    <p>This is a test website built with laravel</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a>
+                </div>
+            </div>
+        </div> <!-- /.row jumbotron-->
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere debitis provident incidunt perferendis! Quisquam magnam cumque reiciendis asperiores nihil soluta repellendus. Dolorem culpa, alias sapiente sequi reprehenderit ullam minima.</p>
+                    <a class="btn btn-primary" href="#">Read More</a>
+                </div>
+
+                <hr class="my-4">
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere debitis provident incidunt perferendis! Quisquam magnam cumque reiciendis asperiores nihil soluta repellendus. Dolorem culpa, alias sapiente sequi reprehenderit ullam minima.</p>
+                    <a class="btn btn-primary" href="#">Read More</a>
+                </div>
+
+                <hr class="my-4">
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere debitis provident incidunt perferendis! Quisquam magnam cumque reiciendis asperiores nihil soluta repellendus. Dolorem culpa, alias sapiente sequi reprehenderit ullam minima.</p>
+                    <a class="btn btn-primary" href="#">Read More</a>
+                </div>
+
+                <hr class="my-4">
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere debitis provident incidunt perferendis! Quisquam magnam cumque reiciendis asperiores nihil soluta repellendus. Dolorem culpa, alias sapiente sequi reprehenderit ullam minima.</p>
+                    <a class="btn btn-primary" href="#">Read More</a>
+                </div>
+
+                <hr class="my-4">
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere debitis provident incidunt perferendis! Quisquam magnam cumque reiciendis asperiores nihil soluta repellendus. Dolorem culpa, alias sapiente sequi reprehenderit ullam minima.</p>
+                    <a class="btn btn-primary" href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-md-3 offset-md-1">
+                <h2>Sidebar</h2>
+            </div>
+        </div> <!-- /.row post -->
+
     </div>
 
     <!-- Optional JavaScript -->
