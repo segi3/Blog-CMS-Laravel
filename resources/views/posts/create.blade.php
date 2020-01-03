9eb0f0f51de92553d0ca2ae8ffbf585ee2ccb848
segitiga <rafi.nizar15@gmail.com>
2020-01-03 13:09:24 +0700

    Update create.blade.php
    added parsley validator

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,24 +3,24 @@
 @section('title', '| Create Post')
 
 @section('stylesheets')
-
+{!! Html::style('css/parsley.css') !!}
 @endsection
 
 @section('content')
 
     <div class="row">
         <div class="col-md-8 offset-md-2">
-        
+
             <h1>Create New Post</h1>
             <hr>
 
-            {!! Form::open(['route' => 'posts.store']) !!}
+            {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
 
                 {{ Form::label('title', 'Title:') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
                 {{ Form::label('body', 'Post Body:') }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
                 
                 {{ Form::submit('Create New Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 10px')) }}
 
@@ -55,5 +55,5 @@
 @endsection
 
 @section('scripts')
-
+{!! Html::script('js/parsley.min.js') !!}
 @endsection
