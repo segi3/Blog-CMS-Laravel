d7957f13ab9c50a8952f482fb7921ed17580132e
segitiga <rafi.nizar15@gmail.com>
2020-01-03 11:38:42 +0700

    added laravelcollective
    for form helper

--- config/app.php
+++ config/app.php
@@ -163,6 +163,12 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Package for laravelcollective
+         */
+
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
          * Package Service Providers...
          */
 
@@ -225,7 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
