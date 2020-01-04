5feccd458a23a7c87e8d872498bf39d83dd87e9a
segitiga <rafi.nizar15@gmail.com>
2020-01-04 14:20:56 +0700

    Create show.blade.php

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,41 @@
+@extends('main')
+
+@section('title', '| View Post')
+
+@section('content')
+        <div class="row">
+            <div class="col-md-8">
+                <h1>{{ $post->title }}</h1>
+                <p class="lead">{{ $post->body }}</p>
+            </div>
+        
+
+            <div class="col-md-4">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <dl class="dl-horizontal">
+                            <dt>Created at:</dt>
+                            <dd>{{ date('M j, Y - H:i', strtotime($post-> created_at))}}</dd>
+                        </dl>
+                        <dl class="dl-horizontal">
+                            <dt>Last Updated:</dt>
+                            <dd>{{ date('M j, Y - H:i', strtotime($post-> updated_at)) }}</dd>
+                        </dl>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                                <!-- <a href="#" class="btn btn-primary btn-block">Edit</a> -->
+                            </div>
+                            <div class="col-sm-6">
+                                {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                                <!-- <a href="#" class="btn btn-danger btn-block">Delete</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+@endsection
+
