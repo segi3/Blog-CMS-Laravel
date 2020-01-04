39ba931a366caf03d08eac4068cae10257e97b59
segitiga <rafi.nizar15@gmail.com>
2020-01-04 14:20:27 +0700

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,14 +14,12 @@
 @endsection
 
 @section('content')
-    <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <h1>About me</h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, cum modi. Fugit cum similique quod fuga excepturi eum, ipsum debitis doloribus, saepe dignissimos laborum est quia unde magni perferendis praesentium.</p>
             </div>
         </div>
-    </div>
 @endsection
 
 @section('scripts')
