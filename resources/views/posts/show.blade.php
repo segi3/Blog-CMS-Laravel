809fbee0f9eeaa47d68df87b218d82bfd505e4ac
segitiga <rafi.nizar15@gmail.com>
2020-01-09 16:46:09 +0700

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,10 @@
                 <div class="card bg-light">
                     <div class="card-body">
                         <dl class="dl-horizontal">
+                            <dt>Url:</dt>
+                            <dd><a href="{{ route('blog.single',$post->slug) }}">{{ url('blog/'.$post->slug) }}</a></dd>
+                        </dl>
+                        <dl class="dl-horizontal">
                             <dt>Created at:</dt>
                             <dd>{{ date('M j, Y - H:i', strtotime($post-> created_at))}}</dd>
                         </dl>
@@ -25,11 +29,16 @@
                         <div class="row">
                             <div class="col-sm-6">
                                 {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
-                                <!-- <a href="#" class="btn btn-primary btn-block">Edit</a> -->
                             </div>
                             <div class="col-sm-6">
-                                {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
-                                <!-- <a href="#" class="btn btn-danger btn-block">Delete</a> -->
+                                {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'DELETE']) !!}
+
+                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-block']) !!}
+
+                                {!! Form::close() !!}
+                            </div>
+                            <div class="col-md-12 mt-2">
+                                <a href="{{ route('posts.index') }}" class="btn btn-dark btn-block"><< back to index</a>
                             </div>
                         </div>
                     </div>
