039cba9836e5747a21fff0af955fcaedbe8db67c
segitiga <rafi.nizar15@gmail.com>
2020-01-09 16:46:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::get('/', 'PagesController@getIndex');
 Route::get('about', 'PagesController@getAbout');
 Route::get('contact', 'PagesController@getContact');
 
+Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'] )->where('slug', '[\w\d\-\_]+');
+Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@getIndex']);
+
 Route::resource('posts', 'PostController');
