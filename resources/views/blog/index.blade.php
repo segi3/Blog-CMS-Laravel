40edeb58a347ece27f4f86355213410818ac11b9
segitiga <rafi.nizar15@gmail.com>
2020-01-09 16:45:44 +0700

    Create index.blade.php

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,45 @@
+@extends('main')
+
+@section('title', '| Blog')
+
+@section('stylesheets')
+
+    <style>
+        #show-posts :last-child > hr {
+            display: none;
+        }
+    </style>
+
+@endsection
+
+@section('content')
+
+        <div class="row">
+            <div class="com-md-8 offset-md-2">
+                <h1>Blog</h1>
+            </div>
+        </div>
+        
+        <div class="row" id="show-posts">
+        @foreach ($posts as $post)
+            <div class="com-md-8 offset-md-2 mt-5">
+                <h2>{{ $post->title }}</h2>
+                <h5>Published: {{ date('j M Y', strtotime($post->created_at)) }}</h5>
+                <p>{{ substr($post->body, 0, 250) }}{{ strlen($post->body) > 250 ? '...':"" }}</p>
+
+                <a href="{{ route('blog.single', $post->id) }}" class="btn btn-primary">Read More</a>
+
+                <hr class="mt-5">
+            </div>
+        @endforeach
+        </div>
+        
+        <div class="row">
+            <div class="col-md-12">
+                <div class=" d-flex justify-content-center">
+                    {!! $posts->links(); !!}
+                </div>
+            </div>
+        </div>
+
+@endsection
