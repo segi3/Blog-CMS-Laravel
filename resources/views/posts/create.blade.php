37d8da3d80ab295e3d2d7610bddbc23afd49db61
segitiga <rafi.nizar15@gmail.com>
2020-01-14 11:36:46 +0700

    Update create.php added categories

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -22,6 +22,13 @@
                 {{ Form::label('slug', 'Slug:') }}
                 {{ Form::text('slug', null, array('class' => 'form-control', 'required' => '', 'minlength' => '5', 'maxlength' => '255', 'data-parsley-alphadash' => '')) }}
 
+                {{ Form::label('category', 'Category:') }}
+                <select name="category" class="form-control">
+                    @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+
                 {{ Form::label('body', 'Post Body:') }}
                 {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
                 
